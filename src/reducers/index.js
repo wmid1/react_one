851b5184d0597764d7125aa993132fe623ab7a55
wmid1 <34446943+wmid1@users.noreply.github.com>
2018-12-26 13:41:27 +0600

    connect all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import ColumnNameReducer from './ColumnNameReducer';
+import ColumnQuantity from './ColumnQuantity';
+import params from './params';
 
 const allReducers = combineReducers({
   columnNames: ColumnNameReducer,
+  colBox: ColumnQuantity,
+  params,
 });
 
 export default allReducers;
