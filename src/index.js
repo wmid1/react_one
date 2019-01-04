9f0471c1930473b8959166c3cc6f9d4f59577c00
wmid1 <34446943+wmid1@users.noreply.github.com>
2019-01-04 15:16:51 +0600

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import allReducers from './reducers';
 
-const store = createStore(allReducers);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
