813fa447b1f2e889b6ab5a6087f066389b681d72
wmid1 <34446943+wmid1@users.noreply.github.com>
2019-01-06 23:40:11 +0600

    use spread operator

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -3,10 +3,16 @@ const initialState = { username: '', start: true };
 export default function(state = initialState, action) {
   switch (action.type) {
     case 'SET_USERNAME':
-      return Object.assign({}, state, { username: action.payload });
+      return {
+        ...state,
+        username: action.payload,
+      };
 
     case 'START_END':
-      return Object.assign({}, state, { start: action.payload });
+      return {
+        ...state,
+        start: action.payload,
+      };
 
     default:
       return state;
