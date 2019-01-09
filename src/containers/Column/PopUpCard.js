b680d583b5fe1689e53d37d87e409e6c1d1627a1
wmid1 <34446943+wmid1@users.noreply.github.com>
2019-01-09 22:36:20 +0600

    Update PopUpCard.js

--- src/containers/Column/PopUpCard.js
+++ src/containers/Column/PopUpCard.js
@@ -61,7 +61,7 @@ class PopUpCard extends Component {
               <button
                 className="  justify-content-md-end close align-top mr-2"
                 aria-label="Close"
-                onClick={() => this.close(columnId, indexCard)}
+                onClick={() => this.close(columnId, cardId)}
               >
                 <span aria-hidden="true">&times;</span>
               </button>
